58cb2ac89af10bf61edc04f217fd59e4576a8871
Mateusz Wiecek <mateusz.wiecek@interia.eu>
2023-05-08 17:52:57 +0200

    car flow fix

--- src/main/java/app/backend/controller/connection/ConnectionController.java
+++ src/main/java/app/backend/controller/connection/ConnectionController.java
@@ -2,7 +2,9 @@ package app.backend.controller.connection;
 
 import app.backend.document.CarFlow;
 import app.backend.service.CarFlowService;
+import app.backend.service.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
@@ -12,40 +14,6 @@ import java.time.LocalTime;
 public class ConnectionController {
 
     @Autowired
-    private CarFlowService carFlowService;
-
-    @GetMapping(value="/get/{id}")
-    public CarFlow getCarFlow(
-            @PathVariable("id") String id
-    ) throws Exception {
-        return carFlowService.getCarFlowById(id);
-    }
-
-    @PutMapping(value="/update/{id}")
-    public CarFlow updateCarFlow(
-            @PathVariable("id") String id,
-            @RequestParam("carFlowPm") int carFlowPm,
-            @RequestParam("startTime") LocalTime startTime,
-            @RequestParam("endTime") LocalTime endTime
-    ) throws Exception {
-        return carFlowService.updateCarFlow(id, carFlowPm, startTime, endTime);
-    }
-
-    @PostMapping(value="/add")
-    public CarFlow addCarFlow(
-            @RequestParam("carFlowPm") int carFlowPm,
-            @RequestParam("startTime") LocalTime startTime,
-            @RequestParam("endTime") LocalTime endTime
-    ) {
-        return carFlowService.addCarFlow(carFlowPm, startTime, endTime);
-    }
-
-    @DeleteMapping(value="/delete/{id}")
-    public CarFlow deleteCarFlow(
-            @PathVariable("id") String id
-    ) throws Exception {
-        System.out.println(id);
-        return carFlowService.deleteCarFlowById(id);
-    }
+    private ConnectionService connectionService;
 
 }
