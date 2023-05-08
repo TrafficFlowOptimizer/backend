46efa81749f626252c07a5fa612da0e988310358
superior_shrimp <szymon.zychowicz300701@gmail.com>
2023-05-08 17:38:36 +0200

    kolejne testy dodane

--- src/test/java/app/backend/service/UserServiceTest.java
+++ src/test/java/app/backend/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package app.backend.service;
 
 import app.backend.document.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,13 +45,116 @@ class UserServiceTest {
         registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
     }
 
-    @Test
-    public void testTest() {
-        User user = userService.addUser("John", "Doe", "JDoe", "password@123");
+    @AfterEach
+    public void cleanUpEach(){
+        userService.userRepository.deleteAll();
+    }
 
-        assertEquals(user.getFirstName(), "John");
-        assertEquals(user.getLastName(), "Doe");
-        assertEquals(user.getNickname(), "JDoe");
-        assertEquals(user.getPassword(), "password@123");
+    @Test
+    public void getUserById_nonExistentUser_userNotFound() {
+        String id = "";
+        Exception exception = assertThrows(Exception.class, () -> {
+            userService.getUserById(id);
+        });
+
+        assertEquals("Cannot get user with id: " + id + " because it does not exist.", exception.getMessage());
+    }
+
+    @Test
+    public void getUserById_existingUser_correctUser() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String nickname = "JDoe";
+        String password = "password@123";
+
+        User user = userService.addUser(firstName, lastName, nickname, password);
+        User found = null;
+        try {
+            found = userService.getUserById(user.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(found);
+        assertEquals(firstName, found.getFirstName());
+        assertEquals(lastName, found.getLastName());
+        assertEquals(nickname, found.getNickname());
+        assertEquals(password, found.getPassword());
+    }
+
+    @Test
+    public void getUserById_multipleUsers_correctUser() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String nickname = "JDoe";
+        String password = "password@123";
+
+        User user = userService.addUser(firstName, lastName, nickname, password);
+        userService.addUser("Notjohn", "Notdoe", "stillnotJD", "password@123");
+        User found = null;
+        try {
+            found = userService.getUserById(user.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(found);
+        assertEquals(firstName, found.getFirstName());
+        assertEquals(lastName, found.getLastName());
+        assertEquals(nickname, found.getNickname());
+        assertEquals(password, found.getPassword());
+    }
+
+    @Test
+    public void addUser_properUser_userAdded() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String nickname = "JDoe";
+        String password = "password@123";
+
+        User user = userService.addUser(firstName, lastName, nickname, password);
+
+        assertEquals(firstName, user.getFirstName());
+        assertEquals(lastName, user.getLastName());
+        assertEquals(nickname, user.getNickname());
+        assertEquals(password, user.getPassword());
+    }
+
+    @Test
+    public void deleteUserById_properUser_userDeleted() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String nickname = "JDoe";
+        String password = "password@123";
+
+        User user = userService.addUser(firstName, lastName, nickname, password);
+        String id = user.getId();
+
+        try {
+            userService.deleteUserById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            userService.getUserById(id);
+        });
+
+        assertEquals("Cannot get user with id: " + id + " because it does not exist.", exception.getMessage());
+    }
+
+    @Test
+    public void deleteUserById_nonExistentUser_userNotFound() {
+        String firstName = "John";
+        String lastName = "Doe";
+        String nickname = "JDoe";
+        String password = "password@123";
+
+        User user = userService.addUser(firstName, lastName, nickname, password);
+        String id = "";
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            userService.deleteUserById(id);
+        });
+
+        assertEquals("Cannot delete user with id: " + id + " because it does not exist.", exception.getMessage());
     }
 }
