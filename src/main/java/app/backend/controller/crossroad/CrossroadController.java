7bd6d9a6241904e7c402f3eb9ccd84d7315187d6
Nikodem Korohoda <84533152+Loloxon@users.noreply.github.com>
2023-10-26 22:15:01 +0200

    BE-OT communication (#16)
    * change structure sent to OT JSON -> object OptimizationRequest
    
    * backend sending REST to OT
    
    * fixed http error handling

--- src/main/java/app/backend/controller/crossroad/CrossroadController.java
+++ src/main/java/app/backend/controller/crossroad/CrossroadController.java
@@ -3,32 +3,21 @@ package app.backend.controller.crossroad;
 import app.backend.authentication.JwtUtil;
 import app.backend.document.crossroad.Crossroad;
 import app.backend.request.crossroad.CrossroadDescriptionRequest;
+import app.backend.request.optimization.OptimizationRequest;
 import app.backend.response.crossroad.CrossroadDescriptionResponse;
-import app.backend.service.CollisionService;
-import app.backend.service.ConnectionService;
-import app.backend.service.CrossroadService;
-import app.backend.service.ImageService;
-import app.backend.service.OptimizationService;
-import app.backend.service.RoadService;
-import app.backend.service.TrafficLightService;
-import app.backend.service.UserService;
+import app.backend.service.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.UnknownHttpStatusCodeException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -43,8 +32,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import static java.lang.Thread.sleep;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @CrossOrigin("*")
@@ -264,7 +252,7 @@ public class CrossroadController {
     ) {
         String result = "{}";
         try (Socket socket = new Socket(OPTIMIZER_HOST, OPTIMIZER_PORT)) {
-            JSONObject jsonData = crossroadsUtils.parseJSON(crossroadId, time);
+            JSONObject jsonData = crossroadsUtils.getJsonData(crossroadId, time);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(jsonData);
 
@@ -310,14 +298,15 @@ public class CrossroadController {
         return ar;
     }
 
-    @GetMapping(value = "/{crossroadId}/optimization/novid/{time}", produces = MediaType.APPLICATION_JSON_VALUE) // TODO
-    public String getOptimizationWithoutVideo(@PathVariable String crossroadId, @PathVariable int time) {
+    @GetMapping(value = "/{crossroadId}/optimization/novid/socket/{time}", produces = MediaType.APPLICATION_JSON_VALUE)
+    // TODO
+    public String getOptimizationWithoutVideoSocket(@PathVariable String crossroadId, @PathVariable int time) {
 
         // TODO: use OptimizationRequest class and create OptimizationResponse class
 
         String result = "{results: \"ERROR\"}";
         try (Socket socket = new Socket(OPTIMIZER_HOST, OPTIMIZER_PORT)) {
-            JSONObject jsonData = crossroadsUtils.parseJSON(crossroadId, time);
+            JSONObject jsonData = crossroadsUtils.getJsonData(crossroadId, time);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(jsonData);
 
@@ -341,6 +330,32 @@ public class CrossroadController {
         System.out.println(result);
 
         return result;
+    }
+
+
+    @GetMapping(value = "/{crossroadId}/optimization/novid/{time}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getOptimizationWithoutVideo(@PathVariable String crossroadId, @PathVariable int time) {
+//        OptimizationRequest optimizationRequest = crossroadsUtils.getOptimizationRequest(crossroadId, time);
+        OptimizationRequest optimizationRequest = new OptimizationRequest();
+
+        String url = "http://localhost:9091/optimization";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<OptimizationRequest> requestEntity = new HttpEntity<>(optimizationRequest, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        } catch (HttpClientErrorException exception) {
+            return ResponseEntity.status(BAD_REQUEST).body("Invalid data given to optimizer\n");
+        } catch (HttpServerErrorException exception) {
+            return ResponseEntity.status(SERVICE_UNAVAILABLE).body("Optimizer unavailable\n");
+        } catch (UnknownHttpStatusCodeException exception) {
+            return ResponseEntity.status(NOT_FOUND).body("Something weird happened\n");
+        }
+        return response;
     }
 
     @GetMapping(value = "/{crossroadId}/optimization_results", produces = MediaType.APPLICATION_JSON_VALUE) // TODO
